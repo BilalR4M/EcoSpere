0f1f3ceca772b48b250db9569f73025109a90ae0
Bilal Awshid <165455288+BilalR4M@users.noreply.github.com>
2024-10-07 08:45:47 +0530

    "auth complete"

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,26 @@
-import 'package:ecosphere/authentication_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ecosphere/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const Ecosphere());
 
 }
 
-class MyApp extends StatelessWidget {
+class Ecosphere extends StatelessWidget {
+  const Ecosphere({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthenticationService>(
-          create: (_) => AuthenticationService(FirebaseAuth.instance),
-        ),
-        StreamProvider(
-          create: (context) => context.read<AuthenticationService>()
-        )
-      ],
-      child: MaterialApp(
-        title: 'Firebase Meetup',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const AuthenticationWrapper(),
+    return MaterialApp(
+      title: 'Ecosphere',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      debugShowCheckedModeBanner: false,
+      home: Login(),
     );
-  }
-}
-
-class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
